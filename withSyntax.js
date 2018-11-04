3446949355a76435519be04ad7b46cbb774ad924
Husny Ahamed <gazzali@omobio.net>
2018-11-04 13:23:56 +0530

    Some comments with keyword

--- withSyntax.js
+++ withSyntax.js
@@ -1,5 +1,7 @@
 // With syntax will ease the object property modifications
 // and will be easy to handle data in object
+// ***This is very dangerous than eval(), because with keyword will create a whole new lexical scope***
+// ** if with keyword present, the compiler disables its optimizations as it has to prepare for worst case **
 
 var obj = {
   a: 1,
@@ -8,13 +10,19 @@ var obj = {
 };
 
 // harder way
-obj.a = obj.b * obj.c;
+obj.a = obj.b + obj.c;
 console.log(obj);
 
 // using with keyword
 with (obj) {
-  a = b * c + 1;
-  c = 101;
+  a = b + c;
+  d = 101;
+  d = "goFish";
 }
 
 console.log(obj);
+
+// d will not be initialized inside obj scope, instead it will go to global scope
+console.log(obj.d);
+
+console.log(d);
